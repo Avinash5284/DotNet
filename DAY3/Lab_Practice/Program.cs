4cfac3d5b93631b10d5f4169ac85d0cf57976707
Avinash5284 <65229426+Avinash5284@users.noreply.github.com>
2020-12-03 09:22:20 +0530

    Add files via upload

---
+++ DAY3/Lab_Practice/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // derivedclass o = new derivedclass();
+            // baseclass o = new baseclass();
+
+            //baseclass o = new derivedclass();
+
+            baseclass o = new subderivedclass();
+            o.display1();
+            o.display();
+            Console.ReadLine();
+        }
+    }
+    public class baseclass
+    {
+
+        public  void display1()
+        {
+
+            Console.WriteLine("base display1");
+        }
+        public virtual  void display()
+        {
+
+            Console.WriteLine("base display");
+        }
+
+    }
+    public class derivedclass : baseclass
+    {
+        public void display1()
+        {
+
+            Console.WriteLine("derived display");
+        }
+        public override void display()
+        {
+
+            Console.WriteLine("derived display");
+        }
+
+    }
+    public class subderivedclass : derivedclass
+    {
+        public void display1()
+        {
+
+            Console.WriteLine("subderived display");
+        }
+        public override void display()
+        {
+
+            Console.WriteLine("subderived  overide display");
+        }
+
+    }
+}
